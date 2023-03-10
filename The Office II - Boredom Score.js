2aa85ca0355638163fe0d0b42f26ba85d1c6dd62
Olga Lubsanova <lubsanova07@gmail.com>
2023-03-10 10:48:08 -0600

    The Office II - Boredom Score

---
+++ The Office II - Boredom Score.js
@@ -0,0 +1,44 @@
+// DESCRIPTION:
+// Every now and then people in the office moves teams or departments. Depending what people are doing with
+// their time they can become more or less boring. Time to assess the current team.
+//
+// You will be provided with an object(staff) containing the staff names as keys, and the department they work in as values.
+//
+// Each department has a different boredom assessment score, as follows:
+//
+// accounts = 1
+// finance = 2
+// canteen = 10
+// regulation = 3
+// trading = 6
+// change = 6
+// IS = 8
+// retail = 5
+// cleaning = 4
+// pissing about = 25
+//
+// Depending on the cumulative score of the team, return the appropriate sentiment:
+//
+// <=80: 'kill me now'
+// < 100 & > 80: 'i can handle this'
+// 100 or over: 'party time!!'
+//
+// SOLUTION:
+
+const score = {
+    'accounts': 1,
+    'finance': 2,
+    'canteen': 10,
+    'regulation': 3,
+    'trading': 6,
+    'change': 6,
+    'IS': 8,
+    'retail': 5,
+    'cleaning': 4,
+    'pissing about': 25,
+}
+
+function boredom(staff){
+    let x = Object.values(staff).map(el => score[el]).reduce((a,b) => a+b, 0)
+    return (x <= 80) ? 'kill me now' : (x > 80 && x < 100) ? 'i can handle this' : 'party time!!'
+}
